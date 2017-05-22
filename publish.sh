33dd4a34e7aac6c600dca6fea4271105d44ef80e
Phoebe Simon <phoebesimon@gmail.com>
2017-05-22 18:54:17 +0000

    Get publish.sh working

--- publish.sh
+++ publish.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 VERSION=$(git describe --tags)
+echo "Publishing $VERSION..."
 
-gox -ldflags "-X main.Version $VERSION -X" -output "dist/{{.OS}}_{{.ARCH}}/burrow-exporter"
-
-rm -rf releases
+mkdir dist
 mkdir releases
+gox -osarch="linux/amd64" -osarch="linux/386" -osarch="darwin/amd64" -osarch="freebsd/amd64" -osarch="freebsd/386" -ldflags "-X main.Version=$VERSION" -output "dist/{{.OS}}_{{.Arch}}/burrow-exporter"
 
-for i in src/* ; do
+for i in dist/* ; do
   if [ -d "$i" ]; then
    ARCH=$(basename "$i")
-   zip releases/burrow-exporter-$VERSION-$ARCH.zip dist/$ARCH/burrow-exporter
+   zip releases/burrow-exporter_$VERSION_$ARCH.zip dist/$ARCH/burrow-exporter
   fi
 done
 
-ghr -t $GITHUB_TOKEN -u jirwin -r burrow-exporter --replace `git describe --tags` dist/
+ghr -t $GITHUB_TOKEN -u jirwin -r burrow_exporter --replace $VERSION releases/
+
+rm -rf dist
+rm -rf releases
