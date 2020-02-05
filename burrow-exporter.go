df4410eb571f9fadffe4296182ccb1e991874d5c
Roger Hwang <rhwang@yext.com>
2020-02-05 15:19:42 -0500

    updating EnvVar to EnvVars

--- burrow-exporter.go
+++ burrow-exporter.go
@@ -24,58 +24,58 @@ func main() {
 		cli.StringFlag{
 			Name:   "burrow-addr",
 			Usage:  "Address that burrow is listening on",
-			EnvVar: "BURROW_ADDR",
+			EnvVars: []string{"BURROW_ADDR"},
 		},
 		cli.StringFlag{
 			Name:   "metrics-addr",
 			Usage:  "Address to run prometheus on",
-			EnvVar: "METRICS_ADDR",
+			EnvVars: []string{"METRICS_ADDR"},
 		},
 		cli.IntFlag{
 			Name:   "interval",
 			Usage:  "The interval(seconds) specifies how often to scrape burrow.",
-			EnvVar: "INTERVAL",
+			EnvVars: []string{"INTERVAL"},
 		},
 		cli.IntFlag{
 			Name:   "api-version",
 			Usage:  "Burrow API version to leverage",
 			Value:  2,
-			EnvVar: "API_VERSION",
+			EnvVars: []string{"API_VERSION"},
 		},
 		cli.BoolFlag{
 			Name:   "skip-partition-status",
 			Usage:  "Skip exporting the per-partition status",
-			EnvVar: "SKIP_PARTITION_STATUS",
+			EnvVars: []string{"SKIP_PARTITION_STATUS"},
 		},
 		cli.BoolFlag{
 			Name:   "skip-group-status",
 			Usage:  "Skip exporting the per-group status",
-			EnvVar: "SKIP_GROUP_STATUS",
+			EnvVars: []string{"SKIP_GROUP_STATUS"},
 		},
 		cli.BoolFlag{
 			Name:   "skip-partition-lag",
 			Usage:  "Skip exporting the partition lag",
-			EnvVar: "SKIP_PARTITION_LAG",
+			EnvVars: []string{"SKIP_PARTITION_LAG"},
 		},
 		cli.BoolFlag{
 			Name:   "skip-partition-current-offset",
 			Usage:  "Skip exporting the current offset per partition",
-			EnvVar: "SKIP_PARTITION_CURRENT_OFFSET",
+			EnvVars: []string{"SKIP_PARTITION_CURRENT_OFFSET"},
 		},
 		cli.BoolFlag{
 			Name:   "skip-partition-max-offset",
 			Usage:  "Skip exporting the partition max offset",
-			EnvVar: "SKIP_PARTITION_MAX_OFFSET",
+			EnvVars: []string{"SKIP_PARTITION_MAX_OFFSET"},
 		},
 		cli.BoolFlag{
 			Name:   "skip-total-lag",
 			Usage:  "Skip exporting the total lag",
-			EnvVar: "SKIP_TOTAL_LAG",
+			EnvVars: []string{"SKIP_TOTAL_LAG"},
 		},
 		cli.BoolFlag{
 			Name:   "skip-topic-partition-offset",
 			Usage:  "Skip exporting topic partition offset",
-			EnvVar: "SKIP_TOPIC_PARTITION_OFFSET",
+			EnvVars: []string{"SKIP_TOPIC_PARTITION_OFFSET"},
 		},
 	}
 
